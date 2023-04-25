45f3de43eee1767a50c769e03704551c20e42404
Laurel Perkins <lmperkins20@gmail.com>
2023-04-25 13:07:42 -0700

    lab-16 complete

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# cloud-server
+# AWS Cloud Server
+
+## Deployed links
+
+* GUI Deploy:
+    * <http://cloud-server.eba-swckykuz.us-west-2.elasticbeanstalk.com/>
+
+* CLI delploy:
+    * <http://cloud-server-3-env.eba-w7hcdttx.us-west-2.elasticbeanstalk.com/>
---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict'
+// very basic working app
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { Server } = require('socket.io');
+const http = require('http');
+
+const app = express();
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('./public'));
+
+app.get('/status', (request, response, next) => {
+  response.status(200).send('Okie dokie');
+});
+
+io.on('connection', (socket) => {
+  console.log('A Socket has connected!', socket.id);
+  socket.on('test', (payload) => {
+    socket.emit('test', payload);
+  });
+});
+
+module.exports = {
+  app,
+  start: (PORT) => {
+    httpServer.listen(PORT, () => {
+      console.log('App is running!');
+    });
+  }
+}
